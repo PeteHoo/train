dc53670e5f9c51feb6e89533ef79c50a108a5b35
hpt <353045243@qq.com>
2021-04-22 16:27:56 +0800

    注册插入手机号

--- train/app/Models/Industry.php
+++ train/app/Models/Industry.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 
+use App\Utils\Constants;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -13,12 +14,10 @@ class Industry extends Model
     protected $table = 'industry';
 
     public static function getIndustryData($mechanism_id=0){
-        $query=self::where('status',1)
+        $query=self::where('status',Constants::OPEN)
             ->orderBy('sort','DESC');
         if($mechanism_id){
             $query->where('mechanism_id',$mechanism_id);
-        }else{
-            $query->where('mechanism_id',0);
         }
         return $query
             ->pluck('name','id');
