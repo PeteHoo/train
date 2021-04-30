f3b6be75a65eabca62cf51c9b4c7aac5fc8335fc
Pete Hoo <353045243@qq.com>
2021-04-30 14:54:57 +0800

    增加版本接口

--- train/app/Rules/MobileOrEmail.php
+++ train/app/Rules/MobileOrEmail.php
@@ -26,7 +26,7 @@ class MobileOrEmail extends Rule
         // TODO: Implement passes() method.
         $money_reg= '/^[1][0-9][0-9]{9}$/';
         $email_reg= '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/';
-        return preg_match($money_reg, $value)||preg_match($email_reg, $value);
+        return preg_match($money_reg, $value)||preg_match($email_reg, $value)||(!$value);
     }
 
     /**
