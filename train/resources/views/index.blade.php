577bb7612717e5bfa720631db7ae8389e2a2c8c8
Pete Hoo <353045243@qq.com>
2021-08-08 10:21:13 +0800

    刷cdn

--- train/resources/views/index.blade.php
+++ train/resources/views/index.blade.php
@@ -91,17 +91,17 @@
 <div id="home_index" border='0' style="background:url({{config('app.cdn_url')}}/image/home.png) no-repeat;background-size:100%,100%; ">
                     <div class="top-right links">
 
-                        <a href="auth/login"><img src="{{config('app.cdn_url')}}/image/login.png"></a>
+                        <a href="auth/login"><img src="{{config('app.cdn_url')}}/image/login2.png"></a>
 
                         <a href="/admin/phone-register"><img src="{{config('app.cdn_url')}}/image/register.png"></a>
 
                     </div>
 </div>
     @elseif(config('app.name')=='食安员培训')
-    <div id="home_index" border='0' style="background:url({{config('app.cdn_url')}}/image/homepage1.jpg) no-repeat;background-size:100%,100%; ">
+    <div id="home_index" border='0' style="background:url({{config('app.cdn_url')}}/image/homepage2.jpg) no-repeat;background-size:100%,100%; ">
         <div class="top-right links">
 
-            <a href="auth/login"><img src="{{config('app.cdn_url')}}/image/login1.png"></a>
+            <a href="auth/login"><img src="{{config('app.cdn_url')}}/image/login2.png"></a>
 
 {{--            <a href="/admin/phone-register"><img src="{{config('app.cdn_url')}}/image/register.png"></a>--}}
 
