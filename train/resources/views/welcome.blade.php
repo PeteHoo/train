69b71480d695886714c5ad46edd33097b4add3b1
胡鹏涛 <353045243@qq.com>
2021-10-20 10:32:06 +0800

    平台版加备案号

--- train/resources/views/welcome.blade.php
+++ train/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>食品培训</title>
+        <title>{{config('app.name')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -81,20 +81,10 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    {{config('app.name')}}
                 </div>
             </div>
         </div>
     </body>
+    <a href="https://beian.miit.gov.cn/#/Integrated/recordQuery"><div style="margin-top: -40px;color: #ffffff" class="copydiv">网站备案号：粤ICP备2021061044号</div></a>
 </html>
