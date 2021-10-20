a109222efd6ec05c40f89faca7b51169cf90c6e8
胡鹏涛 <353045243@qq.com>
2021-10-20 10:55:43 +0800

    平台版加备案号

--- train/resources/views/welcome.blade.php
+++ train/resources/views/welcome.blade.php
@@ -88,3 +88,10 @@
     </body>
     <a href="https://beian.miit.gov.cn/#/Integrated/recordQuery"><div style="margin-top: -40px;color: #000000;height:100px;text-align:center;" class="copydiv">网站备案号：粤ICP备2021061044号</div></a>
 </html>
+
+<script language="javascript" type="text/javascript">
+    $(function () {
+        window.location.href="admin/home";
+    });
+
+</script>
